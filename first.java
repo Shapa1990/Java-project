c781a73f1e6e69b0c48db1660f090224eec6cb32
Shapa1990 <rupazi2015@gmail.com>
2023-11-14 12:48:36 -0600

    my first java program

---
+++ first.java
@@ -0,0 +1,13 @@
+public class first{
+    public static void main (String[] args){
+        System.out.println("null");
+        int age= 33;
+String Name ="Chicago";
+
+Boolean lightson=true;
+   System.out.println(Name);
+   System.out.println(age);
+   System.out.println(lightson);
+     } 
+
+}
